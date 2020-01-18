4d6c078ac9518ad62436bbb34fa85c80fe0c8cd3
Daniel Chan <dhchan@ucsc.edu>
2020-01-18 05:50:28 -0800

    adjusted to 'Basic 86' Free Online Template

---
+++ main.py
@@ -0,0 +1,21 @@
+# [START gae_python37_app]
+from flask import Flask, render_template
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    c = 0
+    return render_template('index.html',count = c)
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
