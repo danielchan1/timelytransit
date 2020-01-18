676a094d8fca3e28285a1d801172c5678a4f494b
Daniel Chan <dhchan@ucsc.edu>
2020-01-18 07:57:56 -0800

    completed website framework

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # [START gae_python37_app]
 from flask import Flask, render_template
-
+from datetime import datetime, date, time
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -9,8 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    c = 0
-    return render_template('index.html',count = c)
+    MINUTES_TO_BUS_STOP= 10
+    estimate = 0 #+/- minutes from now until bus arrives
+    accuracy = 0
+    dt = datetime.now()
+    return render_template('index.html',MINUTES_TO_BUS_STOP = MINUTES_TO_BUS_STOP, dt = dt)
 
 
 if __name__ == '__main__':
---
+++ static/script.js
@@ -0,0 +1,3 @@
+var minbs = prompt("How far away from the Bus Stop are you? (in Minutes) ");
+var MINUTES_TO_BUS_STOP = minbs
+document.getElementById('minbs').innerHTML = minbs;
