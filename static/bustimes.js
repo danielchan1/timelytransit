168e304171f618936b00214f41f16e1ede74b176
Daniel Chan <dhchan@ucsc.edu>
2020-01-19 00:54:21 -0800

    changes

--- static/bustimes.js
+++ static/bustimes.js
@@ -23,11 +23,11 @@ class timeTabler {
     }
 
     getBusName() {
-        return this.bus;
+        return (this.bus);
     }
 
     getStopName() {
-        return this.stopName;
+        return (this.stopName);
     }
 
     findClosestTimes(time) {
@@ -37,19 +37,19 @@ class timeTabler {
                 closestTime = this.times[i];
             }
         }
-        return closestTime;
+        return (closestTime);
     }
 }
 
 SHMF10 = new timeTabler(459, 1185, 60, 'Bus 10', 'Science Hill');
 SCMMF10 = new timeTabler(465, 1125, 60, 'Bus 10', 'Santa Cruz Metro Center');
 SCMMF10.addMoreTimes(735, 1155, 60);
-CWMF10 = new timeTabler(437, 1157, 30, 'Bus 10', 'Cedar & Walnut');
-HLMF10 = new timeTabler(443, 1163, 30, 'Bus 10', 'High & Laurel');
+CWMF10 = new timeTabler(437, 1157, 30, 'Bus 10', 'Cedar and Walnut');
+HLMF10 = new timeTabler(443, 1163, 30, 'Bus 10', 'High and Laurel');
 HLMF10.addMoreTimes(470, 1190, 30);
 
 SCMMF15 = new timeTabler(435, 1185, 15, 'Bus 15', 'Santa Cruz Metro Center');
-BMMF15 = new timeTabler(446, 831, 30, 'Bus 15', 'Bay & Mission');
+BMMF15 = new timeTabler(446, 831, 30, 'Bus 15', 'Bay and Mission');
 BMMF15.addMoreTimes(846, 1026, 30);
 BMMF15.addMoreTimes(861, 1191, 30);
 SHMF15 = new timeTabler(473, 1203, 30, 'Bus 15', 'Science Hill');
@@ -58,7 +58,7 @@ BMMF15.addMoreTimes(898, 1048, 30);
 
 SCMMF16 = new timeTabler(397, 1327, 30, 'Bus 16', 'Santa Cruz Metro Center');
 SCMMF16.addMoreTimes(472, 1222, 30);
-BMMF16 = new timeTabler(463, 1333, 30, 'Bus 16', 'Bay & Mission');
+BMMF16 = new timeTabler(463, 1333, 30, 'Bus 16', 'Bay and Mission');
 BMMF16.addMoreTimes(478, 1228, 30);
 SHMF16 = new timeTabler(475, 25, 15, 'Bus 16', 'Science Hill');
 BMMF16.addMoreTimes(425, 1265, 15);
@@ -66,7 +66,7 @@ BMMF16.addMoreTimes(1325, 1415, 30);
 
 SCMMF19 = new timeTabler(445, 1075, 30, 'Bus 19', 'Santa Cruz Metro Center');
 SCMMF19.addMoreTimes(1105, 1405, 60);
-BMMF19 = new timeTabler(512, 1082, 30, 'Bus 19', 'Bay & Mission');
+BMMF19 = new timeTabler(512, 1082, 30, 'Bus 19', 'Bay and Mission');
 BMMF19.addMoreTimes(1112, 1412, 60);
 SHMF19 = new timeTabler(524, 1094, 30, 'Bus 19', 'Science Hill');
 SHMF19.addMoreTimes(1124, 1424, 60);
@@ -75,8 +75,8 @@ BMMF19.addMoreTimes(1134, 1434, 60);
 
 SHSS10 = new timeTabler(608, 1028, 60, 'Bus 10', 'Science Hill');
 SCMSS10 = new timeTabler(590, 1010, 60, 'Bus 10', 'Santa Cruz Metro Center');
-CWSS10 = new timeTabler(592, 1012, 60, 'Bus 10', 'Cedar & Walnut');
-HLSS10 = new timeTabler(597, 1017, 60, 'Bus 10', 'High & Laurel');
+CWSS10 = new timeTabler(592, 1012, 60, 'Bus 10', 'Cedar and Walnut');
+HLSS10 = new timeTabler(597, 1017, 60, 'Bus 10', 'High and Laurel');
 HLSS10.addMoreTimes(615, 1035, 60);
 SCMSS10.addMoreTimes(630, 1050, 60);
 
@@ -85,16 +85,15 @@ SCMSS16.addMoreTimes(607, 757, 30);
 SCMSS16.addMoreTimes(784, 1054, 30);
 SCMSS16.addMoreTimes(110, 1100, 60);
 SCMSS16.addMoreTimes(1087, 1387, 30);
-BMSS16 = new timeTabler(495, 1395, 30, 'Bus 16', 'Bay & Mission');
+BMSS16 = new timeTabler(495, 1395, 30, 'Bus 16', 'Bay and Mission');
 SHSS16 = new timeTabler(505, 1405, 30, 'Bus 16', 'Science Hill');
 BMSS16.addMoreTimes(515, 1415, 30);
 
 SCMSS19 = new timeTabler(600, 1140, 60, 'Bus 19', 'Santa Cruz Metro Center');
-BMSS19 = new timeTabler(607, 1147, 60, 'Bus 19', 'Bay & Mission');
+BMSS19 = new timeTabler(607, 1147, 60, 'Bus 19', 'Bay and Mission');
 SHSS19 = new timeTabler(619, 1159, 60, 'Bus 19', 'Science Hill');
 BMSS19.addMoreTimes(629, 1169, 60);
 
-alert ("at least sth happen 1")
 var MF = [
         SCMMF10,
         SCMMF15,
@@ -121,16 +120,16 @@ var SS = [
         BMSS19,
         CWSS10,
     ];
-
+alert ("at least sth happen 1.5");
 function closest(stop) {
-    alert ("at least sth happen 2")
     var time = null;
     var bus = null;
     var currentDate = new Date();
     var current_time = null;
+    var stop = null;
     if (currentDate.getDay() == 6 || currentDate.getDay() == 0) {
         for (i = 0; i < SS.length; i++) {
-            if (stop == SS[i].getStopName() && (time == null || time > SS[i].findClosestTimes())) {
+            if ((stop == SS[i].getStopName()) && (time == null || time > SS[i].findClosestTimes())) {
                 current_time = (currentDate.getHours()*60) + currentDate.getMinutes();
                 time = SS[i].findClosestTimes(current_time);
                 bus = SS[i].getBusName();
@@ -139,14 +138,14 @@ function closest(stop) {
     }
     else {
         for (i = 0; i < MF.length; i++) {
-            if (stop == MF[i].getStopName() && (time == null || time > MF[i].findClosestTimes())) {
+            if ((stop == MF[i].getStopName()) && (time == null || time > MF[i].findClosestTimes())) {
                 current_time = (currentDate.getHours()*60) + currentDate.getMinutes();
                 time = MFS[i].findClosestTimes(current_time);
                 bus = MF[i].getBusName();
             }
         }
     }
-    alert ("at least sth happen 3")
-    var a = Math.floor(current_time-time)
+    alert ("at least sth happen 3");
+    var a = Math.floor(current_time-time);
     alert (bus + " will come to your stop in " + a + " minutes");
 }
